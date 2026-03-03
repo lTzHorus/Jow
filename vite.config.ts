4a34ab70b92ea0015eddcbd5fcfa23f536ea553f
Leonardo Marques <leonardo.lima@gpabr.com>
2026-03-03 16:13:17 -0300

    mensagem

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: '/Jow/', // Nome do seu repositório (case-sensitive!)
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
